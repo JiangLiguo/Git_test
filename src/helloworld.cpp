846541f7ba7af43f9ff6548cb0b246877fe37c51
JiangLiguo <806606209@qq.com>
2018-10-19 22:50:42 +0800

    JLG: conflict

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[])
     std::cout<<"Sumbit twist\n";
     std::cout<< "Changed again\n";
     std::cout<<"Changed aaa\n";
-    
+
+    std::cout<<"May be conflict\n";
     return 0;
 }
